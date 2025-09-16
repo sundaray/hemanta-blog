42ac42d826b6d5150784ec3425821abcd85419d7
Hemanta Sundaray <pgcim14.hemant@spjimr.org>
2025-09-16 16:32:13 +0530

    ripple animation added

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
-import { TailwindButton } from "@/components/ui/tailwindbutton";
 
 export default function HomePage() {
   return (
     <div className="m-10">
-<Button>
-            Sign in
+<Button
+      >
+        Sign in
       </Button>
     </div>
   );
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,7 +15,7 @@ type Ripple = {
 }
 
 const buttonVariants = cva(
-  "relative overflow-hidden inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-semibold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "relative overflow-hidden inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-base font-semibold transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {  
@@ -50,6 +50,7 @@ function Button({
   variant,
   size,
   asChild = false,
+  ref,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
@@ -81,6 +82,7 @@ function Button({
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      ref={ref}
       {...props}
       onClick={handleClick}
     >
@@ -93,7 +95,7 @@ function Button({
             initial={{ width: 0, height: 0, opacity: 0.5 }}
             animate={{ width: 400, height: 400, opacity: 0 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 1, ease: "easeOut" }}
+            transition={{ duration: 0.7, ease: "easeOut" }}
             onAnimationComplete={() => handleAnimationComplete(key)}
           />
         ))}
